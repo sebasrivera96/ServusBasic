8f1e39707c334de16e5db2546983bf4a07ea3211
Sebastian Rivera <sebas.rivera96@gmail.com>
2018-10-09 22:10:24 -0500

    Resolve conflicts on the parsing side to run a test program
    The conflicts solved within this commit are:
    1. The reserved words where written in the reverse order, so what I did
    was to write all my program with lower case letters.
    2. On the definition of the parsing rules, all the ''' where changed
    for """.
    3. p_HEAD was modified to avoid ambiguity when wirting an empty program.

--- gramatikServusBasic.py
+++ gramatikServusBasic.py
@@ -1,24 +1,21 @@
 '''
     TODO:
-    - How are final IDs for the variables handled?
-    - Where do I define logic and arithmetic expressions?
-    - How to define the ID of a variable on the lexer side?
+    - Add the UMINUS token for negative numbers
 '''
 import ply.lex as lex
 import ply.yacc as yacc
+from symbolTable import *
 import sys
 
-testProgram = '''
-# Este es un programa de prueba
-    START
-    FREI;
-    DIM A1, A2, B1, B2 als float;
-    LASS A1 <- 10.0;
-    DIM MAT1 als float[100][100];
-    DIM MAT2 als float[100][100];
-    DIM MAT3 als float[100][100];
-    ENDE
-'''
+servusSymbolTable = SymbolTable() 
+
+testProgram = ''' start; 
+frei; 
+dim A1, A2, B1, B2 als float;
+dim MAT1 als float[100][100];
+dim MAT2 als float[100][100];
+dim MAT3 als float[100][100];
+ende; '''
 
 reserved = {
     'start' : 'START', 
@@ -44,35 +41,7 @@ reserved = {
     'waerend' : 'WAEREND'
 }
 
-literals = [
-    '+',
-    '-',
-    '!',
-    '@',
-    '$',
-    '/',
-    '&',
-    '^',
-    '(',
-    ')',
-    '[',
-    ']',
-    '{',
-    '}',
-    '+',
-    '=',
-    '_',
-    '?',
-    ';',
-    ':',
-    ',',
-    '<',
-    '>',
-    '.',
-    '|',
-    '"',
-    '%'
-]
+literals = "+!@$/&^()[]{}+=_?;:,<>.|%"
 
 tokens = [
     'ID', 
@@ -80,11 +49,13 @@ tokens = [
     'STRING',
     'FLOAT_NUMBER',
     'INTEGER_NUMBER',
-    'QUOTATION_MARK',
-    'GrTorE',
-    'SmTorE',
+    'GtE',
+    'StE',
     'EQUAL',
-    'NOT'
+    'NOT',
+    'COMMENT',
+    'QUOTATION_MARK'
+    # 'UMINUS'
 ] + list(reserved.values())
 
 def t_COMMENT(t):
@@ -92,7 +63,7 @@ def t_COMMENT(t):
     pass
 
 def t_ID(t):
-    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    r'[a-zA-Z_][a-zA-Z_\d]*'
     t.type = reserved.get(t.value,'ID')    # Check for reserved words
     return t
 
@@ -112,11 +83,12 @@ def t_STRING(t):
     return t
 
 t_LINKER_PFEIL = r'\<\-'
-t_GrTorE = r'\>\='
-t_SmTorE = r'\<\='
+t_GtE = r'\>\='
+t_StE = r'\<\='
 t_EQUAL = r'\=\='
 t_NOT = r'\!\='
-
+t_QUOTATION_MARK = r'\"'
+# t_UMINUS = r'\-'
 
 # Define a rule so we can track line numbers
 def t_newline(t):
@@ -144,14 +116,13 @@ def testLexer():
 # Here begins the PARSER
 
 def p_HEAD(p):
-    '''
-    HEAD : START S ENDE
-    '''
+    """ HEAD : START ';' S ENDE ';' """
 
 def p_S(p):
-    '''
-    S : instruction
-        | instruction S
+    """
+    S : instruction S
+        | empty
+
     instruction : print 
         | clearScreen
         | if
@@ -163,55 +134,53 @@ def p_S(p):
         | EINGABE input ';'
         | DEF ID '{' S RETURN ';' '}'
         | GOSUB ID ';'
-    '''
+    """
+
+def p_empty(p):
+    'empty :'
+    pass
 
 def p_print(p):
-    '''
+    """
     print : DRUCK ID ';'
         | DRUCK STRING ';'
-    '''
+    """
 
 def p_clearScreen(p):
-    '''
-    clearScreen : FREI ';'
-    '''
+    """ clearScreen : FREI ';' """
 
 def p_if(p):
-    '''
+    """
     if : WENN logicExpression '{' S '}' SONST '{' S '}'
         | WENN logicExpression '{' S '}'
-    '''
+    """
 
 def p_doWhile(p):
-    '''
-    doWhile : TUN '{' S '}' SOLANGE logicExpression ';'
-    '''
+    """ doWhile : TUN '{' S '}' SOLANGE logicExpression ';' """
 
 def p_for(p):
-    '''
+    """
     for : FUR ID LINKER_PFEIL forAssignation IN forTarget '{' S '}'
     forAssignation : arithmeticExpression
         | INTEGER_NUMBER
         | FLOAT_NUMBER
     forTarget : INTEGER_NUMBER
         | FLOAT_NUMBER
-    '''
+    """
 
 def p_let(p):
-    '''
+    """
     let : ID LINKER_PFEIL letAssignation ';'
     letAssignation : arithmeticExpression
         | logicExpression
         | booleanAssignation
-    '''
+    """
 
 def p_while(p):
-    '''
-    while : WAEREND logicExpression '{' S '}'
-    '''
+    """ while : WAEREND logicExpression '{' S '}' """
 
 def p_declareVariable(p):
-    '''
+    """
     declareVariable : ID ',' declareVariable
         | ID ALS type
     type : dataTypes '[' INTEGER_NUMBER ']' '[' INTEGER_NUMBER ']' ';'
@@ -219,55 +188,79 @@ def p_declareVariable(p):
         | dataTypes ';'
     dataTypes : WORT
         | FLOAT
-    '''
+    """
+    # TODO insert new variable to servusSymbolTable
 
 def p_input(p):
-    '''
+    """
     input : ID ',' input
         | ID ';'
-    '''
+    """
+
+precedence = (
+    # LOWEST PRIORITY
+    ('left', '+', '-'),
+    ('left', 'GtE', 'StE', 'EQUAL'),
+    ('left', 'UND', 'ODER', 'NOT'),
+    ('left', '*', '/', '%'),
+    # ('right', 'UMINUS'),
+    # HIGHEST PRIORITY
+)
 
 def p_logicExpression(p):
-    '''
-    logicExpression : '(' logicOperand logicOperator secondLogicOperand ')'
-    secondLogicOperand : logicExpression
-        | logicOperand
-    logicOperand : ID
+    """
+    logicExpression : logicExpression ODER logicExpressionAND
+        | logicExpressionAND
+    logicExpressionAND : logicExpressionAND UND logicExpressionNOT
+        | logicExpressionNOT
+    logicExpressionNOT : logicExpressionNOT NOT logicExpressionGtE
+        | logicExpressionGtE
+    logicExpressionGtE : logicExpressionGtE GtE logicExpressionStE
+        | logicExpressionStE
+    logicExpressionStE : logicExpressionStE StE logicExpressionEq
+        | logicExpressionEq
+    logicExpressionEq : logicExpressionEq EQUAL operand
+        | '(' logicExpression ')'
+        | operand
+
+    operand : ID
         | INTEGER_NUMBER
         | FLOAT_NUMBER
         | STRING
-    logicOperator : '>'
-        | '<'
-        | '&'
-        | '|'
-        | 'GrTorE'
-        | 'SmTorE'
-        | 'EQUAL'
-        | 'NOT'
-        | 'UND'
-        | 'ODER'
-    '''
+    """
 
 def p_arithmeticExpression(p):
-    '''
-    arithmeticExpression : '(' arithOperand arithOperator secondArithOperand ')'
-        | arithOperand
-    secondArithOperand : arithmeticExpression
+    """
+    arithmeticExpression : arithmeticExpression '+' subs
+        | subs
+    subs : subs '-' mul
+        | mul
+    mul : mul '*' mod
+        | mod
+    mod : mod '%' divi
+        | divi
+    divi : divi '/' arithOperand
+        | '(' arithmeticExpression ')'
         | arithOperand
     arithOperand : ID
         | INTEGER_NUMBER
         | FLOAT_NUMBER
-    arithOperator : '+'
-        | '-'
-        | '*'
-        | '/'
-        | '%'
-    '''
+    """
 
 def p_booleanAssignation(p):
-    '''
+    """
     booleanAssignation : logicExpression '?' arithmeticExpression ':' arithmeticExpression
-    '''
+    """
+
+# Error rule for syntax errors
+def p_error(p):
+    print("Syntax error in input! ", p)
 
 # Build the parser
 parser = yacc.yacc()
+
+def testParser():
+    result = parser.parse(testProgram)
+    # print(result)
+
+testParser()
