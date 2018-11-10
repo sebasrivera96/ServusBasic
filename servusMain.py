f2b57065aa7b869dd575e3c5f92212ed2ab7a807
sebasrivera96 <sebas.rivera96@gmail.com>
2018-11-10 12:21:56 -0600

    Modify servusMain to create a file Obj when reading a test program

--- servusMain.py
+++ servusMain.py
@@ -21,50 +21,12 @@ import sys
 
 # ----------------------- HELPER FUNCTIONS -------------------------------------
 
-
-testProgram = """
-start;
-# frei;
-dim f1, f2, f3, i, j, k als float;
-lass f1 <- 8.9 + 3 - 2;
-lass f2 <- 0;
-lass f3 <- 3 + 2;
-lass i <- 0;
-# lass i <- (f3 > f2) ? 0 : f2;
-
-# wenn (f2 == 0) && f3 > 0 {
-#     druck "IF TRUE";
-# }
-# sonst {
-#     druck "IF FALSE";
-# }
-
-# waerend (f2 <= f3){
-#     druck f2;
-#     lass f2  <- f2 + 1;
-# }
-
-# druck "dowhile loop";
-# tun{
-#     lass f3 <- f3 + 3;
-#     druck f3;
-# } solange (f3 < 30);
-
-druck "This is a for loop";
-fur i <- 0 in 2{
-    fur j <- 0 in 3{
-        fur k <- 0 in 3{
-            druck k;
-        }
-        druck j;
-    }
-    druck i;
-}
-ende;
-"""
-
 def testParser():
     result = parser.parse(testProgram)
+
+
+testProgram_fileObj = open("servusProgram2.txt",'r')
+testProgram = testProgram_fileObj.read()
 
 testParser()
 printIntermediateCode()
